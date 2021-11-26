99af1c3007c1c8b761f05fe1db0d7c89cfe9e5d5
guillermoFragachan <guillefragachan@gmail.com>
2021-11-26 10:01:25 +0100

    lets go

---
+++ src/store/reducers/index.js
@@ -0,0 +1,11 @@
+import { initialState } from '../index.js'
+
+
+const mainReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+export default mainReducer
